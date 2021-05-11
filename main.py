7f1fcb64995db95dd69eb46622e212221bf9d514
diacarcor <>
2021-05-11 09:04:11 +0000

    Initial commit

---
+++ main.py
@@ -0,0 +1,14 @@
+# Split string method
+names_string = input("Give me everybody's names, separated by a comma. ")
+names = names_string.split(", ")
+# 🚨 Don't change the code above 👆
+
+#Write your code below this line 👇
+import random
+
+names_size = len(names)
+
+index = random.randint(0,names_size-1)
+
+print(f"{names[index]} is going to buy the meal today!")
+
